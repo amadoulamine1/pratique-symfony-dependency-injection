a7133a7ecc3baf69e8f55c1eb5d8d77d5a5b6e32
Amadou Lamine KEBE <amadoulamine1@gmail.com>
2021-04-27 10:32:43 +0000

    modification database injection de dependance

--- index.php
+++ index.php
@@ -4,15 +4,19 @@ use App\Controller\OrderController;
 use App\Database\Database;
 use App\Mailer\GmailMailer;
 use App\Texter\SmsTexter;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 require __DIR__ . '/vendor/autoload.php';
+$container = new ContainerBuilder();
+$container->set('database', new Database());
 
-$database = new Database();
+$database = $container->get('database');
 $texter = new SmsTexter("service.sms.com", "apikey123");
 $mailer = new GmailMailer("lior@gmail.com", "123456");
 $controller = new OrderController($database, $mailer, $texter);
 
 $httpMethod = $_SERVER['REQUEST_METHOD'];
+
 
 if($httpMethod === 'POST') {
     $controller->placeOrder();
